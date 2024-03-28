d39e560e84af56f8cab8fb0f8406c674ff91718e
313shazid313 <hossainshazid567@gmail.com>
2024-03-28 22:49:52 +0600

    comitted

--- backend/controller/adminController.js
+++ backend/controller/adminController.js
@@ -4,9 +4,11 @@ const getAllUsers = async (req, res) => {
   try {
     const users = await User.find({}, { password: 0 });
     // for getting user data without password
+    
     if (!users || users.length === 0) {
       return res.status(401).json({ message: "No Users Found" });
-    } else {
+    } 
+    else {
       return res.status(200).json(users);
     }
   } catch (error) {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,8 +14,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <NavBar />
-        
+        <NavBar />  
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
